cba9ded238cd999c1f1a850644b3fff3d985ebf8
RLHusted <RLHusted@users.noreply.github.com>
2015-06-21 14:57:23 -0400

    Final submit

--- cachematrix.R
+++ cachematrix.R
@@ -10,14 +10,9 @@ makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     # set x = y to use outside of set function & resets m
     # redefine what is being passed into the main function makeCacheMatrix
-    set <- function(y) {
-        # can use m and x in makeCacheMatrix function
-        x <<- y   
-        m <<- NULL
-    }
     # can use m outside of makeCacheMatrix    
     get <- function() x
-
+    
     # store the value of an imput into a variable to use in makeCacheMatrix
     setinv <- function(solve) m <<- solve
     #return the value stored in m
@@ -27,12 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
     # this is the result of makeCacheMatrix
     # to use assign makeCacheMatrix to an object, then you can use
     # all 4 functions defined.
-    list(set = set, get = get,
+    list( get = get,
          setinv = setinv,
          getinv = getinv)
     
 }
-
 
 ## Retrun the inverse of a defined matrix
 ##  must be square in order to use
@@ -42,12 +36,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     #retreive m
+    #print(x)
     m <- x$getinv()   #$ operator is invalid for atomic vectors
-    #  tried
-    #m <- x[["getinv", exact=FALSE]]   #Error subscript out of bounds
-    #m <- getElement(x,"getinv") #Error in object[[name, exact = TRUE]] : subscript out of bounds
-    #m <- x[["getinv"]]   #Error subscript out of bounds
-    
+   
     # check if mean is already stored and return value as m if it is
     if(!is.null(m)) {
         message("getting cached data")
@@ -65,5 +56,5 @@ cacheSolve <- function(x, ...) {
 
 
 #use
-mdat <- matrix(c(1,2, 11,12), nrow = 2, ncol = 2)
+mdat <- makeCacheMatrix(matrix(c(-1, -2, 1, 1), nrow = 2, ncol = 2))
 cacheSolve(mdat)
